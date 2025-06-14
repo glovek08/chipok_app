2b9aef394c8783ac8bde4c22b8bde87912a9f3d8
glovek08 <gbrielbbarn@gmail.com>
2025-06-14 00:06:08 +0000

    feat: switched to flask debug

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask in Docker!"
+    return "<h1>Chinese Pokedex</h1><small>Cheap ass pokedex from the ghetto.</small>"
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1><small>Scrappy About</small>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
